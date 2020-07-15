398a5c3750fd86cc943653041ebabfe00b6f98fd
jay-web <jay.developer12@gmail.com>
2020-07-15 12:09:17 +0530

    Added removeAt and insertAt methods in linked list

--- LinkedList/db.js
+++ LinkedList/db.js
@@ -61,19 +61,19 @@ class LinkedList {
     // * Method four - return last node of the linked list
 
     getLast(){
-        if(!this.head){
+        if(!this.head){                          // * Check linked list is empty or not
             return null;
         }
 
-        let tHead = this.head;
-        let tail;
+        let tHead = this.head;                  // * Assign head in temporary variable for loop
+        let tail;                               // * Variable to assign last node 
 
-        while(tHead){
-            tail = tHead;
-            tHead = tHead.next;
+        while(tHead){                           // * Loop till tHead is not null
+            tail = tHead;                       // * assign tHead to tail, if tHead is not null
+            tHead = tHead.next;                 // * replace tHead.next to tHead to continue loop 
         }
 
-        return tail;
+        return tail;                            // * return tail i.e last node
     }
 
     // * Method Five - clear the entire linked list means empty it
@@ -93,7 +93,124 @@ class LinkedList {
 
     }
 
+    // * Method Seven - to remove the last node of the linked list
 
+    removeLast() {
+        if(!this.head) return null;             // * check list is empty already or not
+
+        if(!this.head.next){                    // * check list has only single node 
+            this.head = null;
+            return;
+        }
+
+        let previous = this.head;               // * to track previous node
+        let tail = previous.next;               // * to track tail
+        
+        while(tail) {                           // * loop till tail available
+            if(tail.next == null){              // * check node next to tail is null
+                previous.next = null;           // * assign node.next of the node which is second last node i.e previous
+            }
+            previous = tail;                    // * replace the previous node with tail to continue looping        
+            tail = tail.next;                   // * replace the tail node with tail.next to continue looping   
+        }
+
+    }
+
+    // * Method Eight - to add the node at the end of linked list
+
+    insertLast(data) {
+        const node = new Node(data);          // * Create new instance of node using Node class
+
+        if(!this.head){                             // * Check list is already empty
+           return this.head = node;
+        }
+
+        let lastNode = this.getLast();              // * Using getLast function, to get last node
+
+        lastNode.next = node;                       // * Assigning new node to lastnode.next value
+
+
+    }
+
+    // * Method Nine - to get the node of specific index in linked list
+
+    getAt(index) {
+        if(!this.head == null) return;              // * Check list is already empty
+        let tNode = this.head;                      // * Create temporary variable as a head
+        let counter = 0;                            // * Create counter for loop                
+        
+        while(tNode){                    
+            if(counter === index){                  // * Check counter is equal to requested index
+                return tNode;                       // * this will be our output if index available
+            }
+
+            counter++;
+            tNode = tNode.next;
+        }
+
+        return null;                               // * Return the null, if index is not available
+    }
+
+    // * Method Tenth - to remove node at particular index
+
+    removeAt(index) {
+        // * CASE 1
+            // ? if list is empty already
+        if(!this.head) return null;    
+        
+        // * CASE 2
+            // ? If we want to remove head or first node of the linked list
+        if(index == 0){                             
+            this.head = this.head.next;
+            return;
+        }
+
+        // * This is to get the previous node of the required index node
+        // TODO - Review the getAt method of the linked list
+        let previous = this.getAt(index - 1);       
+
+        // * CASE 3 and 4 - Below code will handle two cases
+            // ? if previous index node is not available in linked list, out of bound
+            // ? if previous is available but index is not available
+
+        if(previous == null || previous.next == null){
+            return;
+        }
+
+
+         // * Here we assign next property of previous to the required index.next node
+        previous.next = previous.next.next;    
+       
+    }
+
+    // * Method Eleven - insert new node at requested index
+
+    insertAt(data, index) {
+        // * If linked list is empty
+            // ? So add the new node as a head
+        if(!this.head){
+            this.head = new Node(data);
+            return;
+        }
+
+        // * To handle index 0
+        if(index == 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+ 
+        // * To get the previous node of the requested index
+        let previous = this.getAt(index - 1);
+
+        if(previous == null){
+            let tail = this.getLast();
+            tail.next = new Node(data);
+            return;
+        }
+        previous.next = new Node(data, previous.next);
+
+        
+    }
 }
 
 
--- LinkedList/index.js
+++ LinkedList/index.js
@@ -3,28 +3,67 @@ const LinkedList = require('./db');
 
 const list = new LinkedList();
 
-list.insertFirst(101);
-list.insertFirst(102);
-list.insertFirst(201);
-list.insertFirst(301);
-console.log(list);
+// list.insertFirst(101);
+// list.insertFirst(102);
+// list.insertFirst(201);
+// // list.insertFirst(301);
+// // console.log(list);
 
-console.log("size", list.size());
-
-
-console.log("First Node", list.getFirst());
+// // console.log("size", list.size());
 
 
-console.log("Last Node", list.getLast());
+// // console.log("First Node", list.getFirst());
 
-// list.clear();
 
-// console.log("Size after empty linked list", list.size());
+// // console.log("Last Node", list.getLast());
 
-list.removeFirst();
+// // // list.clear();
 
-list.removeFirst();
+// // // console.log("Size after empty linked list", list.size());
 
-console.log("========================================");
-console.log("size", list.size());
-console.log("list", list);
+// // list.removeFirst();
+
+// // list.removeFirst();
+
+// // console.log("========================================");
+// // console.log("size", list.size());
+// console.log("list", list);
+
+// console.log("last node", list.removeLast());
+
+// console.log("list", list);
+
+// list.removeLast();
+
+// console.log("list", list);
+
+// list.removeLast();
+
+// console.log("list", list);
+
+list.insertLast(1000);
+list.insertLast(2000);
+list.insertLast(3000);
+
+// console.log("list after - insert last", list);
+
+// console.log("getAt", list.getAt(2));
+
+
+// list.removeAt(1);
+
+// console.log("after remove at ", list);
+
+console.log("List ", list);
+
+list.insertAt(6700, 1);
+
+console.log("List ", list);
+
+console.log(list.size());
+
+console.log(list.getAt(2));
+
+list.insertAt(6700, 5);
+
+console.log("List ", list);
